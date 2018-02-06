351a1e53cda354a40458406e9771b7d685e5985c
Nitesh <niteshkmr049@gmail.com>
2018-02-06 11:35:36 +0530

    Core Data Code
    Basic Example for Core Data

--- coreDataProject/ViewController.swift
+++ coreDataProject/ViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    
+    var name: [String] = []
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return name.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tblView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = name[indexPath.row]
+        return cell
+    }
+    
+    
+    @IBOutlet weak var tblView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "The List"
+        tblView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+       
+        let saveAction = UIAlertAction(title: "Save",style: .default) {
+                                       [unowned self] action in
+                                        
+                                        guard let textField = alert.textFields?.first,
+                                            let nameToSave = textField.text else {
+                                                return
+                                        }
+            self.name.append(nameToSave)
+            self.tblView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default)
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
     }
+                                        
+    }
+    
+    
+   
+   
 
 
-}
+
 
